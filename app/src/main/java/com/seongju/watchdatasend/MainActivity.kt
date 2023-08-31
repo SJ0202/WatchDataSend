108008910c1ab2567d6479bbedb2bdc5750251f9
SeongJu <dnflrkrwhr5@naver.com>
2023-08-31 14:40:22 +0900

    Success watch node

--- app/src/main/java/com/seongju/watchdatasend/MainActivity.kt
+++ app/src/main/java/com/seongju/watchdatasend/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.seongju.watchdatasend
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,9 +11,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.seongju.core_watch.client.WatchClientImpl
+import com.seongju.core_watch.common.result.ClientResult
 import com.seongju.watchdatasend.ui.theme.WatchDataSendTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class MainActivity : ComponentActivity() {
+
+    private val serviceScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -26,6 +37,22 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        val watchClient = WatchClientImpl(context = application)
+        watchClient.findNode()
+            .onEach {
+                when(it) {
+                    is ClientResult.Error -> {
+                        Log.d("Activity", "Not find")
+                    }
+                    ClientResult.Success -> Unit
+                }
+            }.launchIn(scope = serviceScope)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceScope.cancel()
     }
 }
 
